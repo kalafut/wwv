b063c7da6a575e5bfc51c0403e2037e105d4943e
Jim Kalafut <jim@kalafut.net>
2018-12-11 07:39:01 -0800

    WIP

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
-var at_the_tone = document.getElementById("at_the_tone"); // start T-7.6s
-var h23 = document.getElementById("h23"); // start T-7.6s
-var m53 = document.getElementById("m53"); // start T-7.6s
-
-function go() {
-    var now = new Date();
-
-    var ms = 1000*now.getSeconds() + now.getMilliseconds();
-    var timeLeft = 60000-ms;
-
-    el.currentTime=80;
-    var wait = timeLeft-(12 * 1000);
-    if(wait < 0) {
-        wait += 60000;
-    }
-    console.log(wait);
-    setTimeout('el.play()', wait);
-    //el.play();
-}
+var at_the_tone = new Audio();
+at_the_tone.src="wwv/at_the_tone.mp3";
+var h23 = document.getElementById("h23");
+var m53 = document.getElementById("m53");
 
 function playAt(el, time) {
     var now = new Date();
@@ -24,14 +9,26 @@ function playAt(el, time) {
     var wait = time - ms;
 
     if(wait >= 0) {
+        console.log(wait);
+        setTimeout(el.play.bind(el), wait);
     }
-
-    console.log(wait);
-    setTimeout(el.play.bind(el), wait);
 }
 
 function stop() {
     document.getElementById("aud0").pause();
+}
+
+/*
+ *
+*/
+function schedule() {
+    t = getTime();
+    var ms = 1000*t.getSeconds() + t.getMilliseconds();
+    
+}
+
+function getTime() {
+    return new Date();
 }
 
 playAt(at_the_tone, 52400);
